d4c540cf70c9293e16b6af962cf02c29ed377d69
Nathan <nathanielvarney@gmail.com>
2012-09-21 18:46:55 -0400

    Changed background. I like it

--- index.php
+++ index.php
@@ -13,31 +13,27 @@
  </head>
  <body>
  	<!--Website heading with gradient background-->
- 	<h1><a class='linkalt' href='http://nathanielvarney.com/dwa/p1'>Nathan Varney</a></h1>
+ 	<h1><a class='no-underline' href='http://nathanielvarney.com/dwa/p1'>Nathan Varney</a></h1>
  	
  	<!--Personal description-->
- 	<div id='
- 	<h2>
- 		About Me
- 	</h2>
- 	<p>
+ 	<div id='about-me'>
+ 		<h2>About Me</h2>
  		I grew up in Orlando, Florida. 
  		This means that I'm usually cold from October to May, but never feel like it's too humid. 
  		I moved to Boston with my family after graduating from the University of South Florida. 
  		I am currently working in IT at the Harvard School of Public Health, where I get to build AV racks and fix broken equipment, computers, and dreams. 
  		In my free time I enjoy running, wandering in the hilarity of the internet, and generally fixing/building/tweaking/keeping-myself-busy-ing. 
  		I am taking this Dynamic Web Applications course because I enjoy learning new skills and I don't have a personal website yet (seriously, it's almost 2013). 
+		<br><br> 	
+ 	</div>
  	
  	<!--Table for the required project information-->
- 	<h2>
- 		Project Info
- 	</h2>	
- 	<div id='info'>
- 		<br>
+ 	<div id='project-info'>
+ 		<h2>Project Info</h2>	
  		<table>
  			<tr class='alt'>
  				<td>Host</td>
- 				<td><a href='http://asmallorange.com'>Asmallorange.com</a></td>
+ 				<td><a class='blacklink' href='http://asmallorange.com'>Asmallorange.com</a></td>
  			</tr>
  			<tr>
  				<td>Plan</td>
@@ -45,7 +41,7 @@
  			</tr>
  			<tr class='alt'>
  				<td>Code Editor</td>
- 				<td><a href='http://www.barebones.com/products/TextWrangler/'>TextWrangler</a></td>
+ 				<td><a class='blacklink' href='http://www.barebones.com/products/TextWrangler/'>TextWrangler</a></td>
  			</tr>
  			<tr>
  				<td>Git Repo</td>
